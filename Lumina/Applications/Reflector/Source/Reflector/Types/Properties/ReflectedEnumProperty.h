fc72f3c6cbff9ad32de506adac28b44b8e26eed9
MrDrElliot <133075253+MrDrElliot@users.noreply.github.com>
2025-06-05 21:43:34 -0500

    :)

---
+++ Lumina/Applications/Reflector/Source/Reflector/Types/Properties/ReflectedEnumProperty.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+#include "ReflectedProperty.h"
+
+
+namespace Lumina
+{
+    class FReflectedEnumProperty : public FReflectedProperty
+    {
+    public:
+        
+        const char* GetTypeName() override;
+        void AppendDefinition(std::stringstream& SS) const override;
+
+    };
+}
